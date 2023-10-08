ae63c8ced4ac681cfa7ae810af514359cb3e46ed
betuuulsan <betulsayin034@gmail.com>
2023-10-08 13:55:02 +0300

    Update hafta3_tkinter.py

--- hafta3_tkinter.py
+++ hafta3_tkinter.py
@@ -1,5 +1,23 @@
 print("merhaba dünya")
-import tkinter
-pencere = tkinter.TK()
+import tkinter as tk
+from tkinter import ttk
+
+pencere = tk.Tk()
+pencere.geometry("300x200")
+pencere.title("Selamlama sayfası")
+
+mesaj = tk.Label(pencere,
+                text="merhaba,dünya",
+                font=("Helvetica",20),
+                bg="#87a889")
+mesaj.pack()
+
+buton = ttk.Button(pencere,
+                   text= "EXIT",
+                   command=lambda: pencere.quit())
+buton.pack()
+
+
+
 
 pencere.mainloop()
